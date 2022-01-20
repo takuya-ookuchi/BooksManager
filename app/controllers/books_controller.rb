44313e3c4e8acbd2a8dd4baf97d63feae4eea077
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-01-20 13:50:08 +0900

    prettierを使用

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,9 @@
 class BooksController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
-  def new
-  end
+  def new; end
 
-  def edit
-  end
+  def edit; end
 end
