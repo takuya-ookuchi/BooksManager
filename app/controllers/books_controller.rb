ce3529363c3ceb8df86853a3f2dd624bf006ef40
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-01-21 14:42:31 +0900

    book新規登録アクションを実装する

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,9 +11,19 @@ class BooksController < ApplicationController
 
   def edit; end
 
-  def create; end
+  def create
+    book = Book.new(book_params)
+    book.save!
+    redirect_to books_url, notice: "書籍「#{book.title}」を登録しました。"
+  end
 
   def destroy; end
 
   def update; end
+
+  private
+
+    def book_params
+      params.require(:book).permit(:title)
+    end
 end
