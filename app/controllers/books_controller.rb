d7265715eed6c768c3fd0fdd747b16aa601ab114
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-01-24 17:37:18 +0900

    不要なallメソッドを削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
 
   def index
-    @q = Book.all.ransack(params[:q])
+    @q = Book.ransack(params[:q])
     @books = @q.result
   end
 
