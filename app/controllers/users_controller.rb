4bb790e4d1de8404b73a73f57b229726a99cd172
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-01-25 15:32:11 +0900

    コントローラの雛形を作成する(user)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  def index; end
+
+  def show; end
+
+  def new; end
+
+  def edit; end
+
+  def create; end
+
+  def destroy; end
+
+  def update; end
+end
