37b2d747fb7e43f4c255eb010d889ba44c33d27a
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-01-25 17:56:09 +0900

    createアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,19 @@ class UsersController < ApplicationController
 
   def edit; end
 
-  def create; end
+  def create
+    user = User.new(user_params)
+    user.save!
+    redirect_to user_url, notice: "「#{user.name}」を登録しました。"
+  end
 
   def destroy; end
 
   def update; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
