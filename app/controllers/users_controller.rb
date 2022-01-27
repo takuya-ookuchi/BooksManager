6ec166da46dc2de17d866a66c7ce2a4f0e408fa2
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-01-27 15:10:12 +0900

    ふりがな情報をpermitに追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :furigana)
   end
 
   def set_user
