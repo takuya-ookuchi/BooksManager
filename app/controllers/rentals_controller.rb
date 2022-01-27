114721041c5200fb5c62bd13b693e33ab3408f34
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-01-27 16:54:43 +0900

    コントローラの雛形を作成する

---
+++ app/controllers/rentals_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class RentalsController < ApplicationController
+  def index; end
+
+  def new; end
+
+  def edit; end
+
+  def create; end
+
+  def update; end
+end
