3c3d47b68a45c4aba082cb3968776e007580b753
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-01-27 15:11:02 +0900

    ふりがなに検証を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
+  validates :furigana, presence: true
+  validates :furigana, length: { maximum: 30 }
 end
