76b7aa5aa45e536093e278538bfd0382a578b248
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-01-31 14:59:05 +0900

    日付を日本語で表示する設定を追加

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +18,6 @@ module Booksmanager
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
   end
 end
