0a4b7877661ce7c53e31959872d2599c1178ca71
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-02-01 16:59:05 +0900

    DBにreturned_atを追加する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_070326) do
+ActiveRecord::Schema.define(version: 2022_02_01_044753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2022_01_27_070326) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "returned_at"
     t.index ["book_id"], name: "index_rentals_on_book_id"
     t.index ["user_id"], name: "index_rentals_on_user_id"
   end
