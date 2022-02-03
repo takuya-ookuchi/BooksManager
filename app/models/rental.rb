9d68728d80184028378888bd3c05878997d29028
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-02-03 11:19:47 +0900

    返却を一度しかできないよう制限する

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 class Rental < ApplicationRecord
+  validate :validate_not_returnable_on_rental_not_going
+
   belongs_to :user
   belongs_to :book
 
   def going?
     returned_at.nil?
+  end
+
+  private
+
+  def validate_not_returnable_on_rental_not_going
+    errors.add(book.title, 'をまだ借りていません。') unless @rental&.returned_at.nil?
   end
 end
