250cf6babe6e5996b6b505b5005eb4f109c3c4e5
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-02-03 12:11:09 +0900

    メソッド名をわかりやすく変更

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,7 +2,7 @@
 
 class Rental < ApplicationRecord
   validate :validate_not_rentable_on_rental_going
-  validate :validate_not_returnable_on_rental_not_going
+  validate :validate_not_returnable_before_rental
 
   belongs_to :user
   belongs_to :book
@@ -17,7 +17,7 @@ class Rental < ApplicationRecord
     errors.add(book.title, 'は貸出中です。借りることはできません。') if @rental&.going?
   end
 
-  def validate_not_returnable_on_rental_not_going
+  def validate_not_returnable_before_rental
     errors.add(book.title, 'をまだ借りていません。') if @rental&.returned_at
   end
 end
