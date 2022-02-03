7241223e586596bc544ca85f5939272586a94191
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-02-03 11:21:08 +0900

    貸し出し中は、貸出できないよう制限する

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Rental < ApplicationRecord
+  validate :validate_not_rentable_on_rental_going
   validate :validate_not_returnable_on_rental_not_going
 
   belongs_to :user
@@ -11,6 +12,10 @@ class Rental < ApplicationRecord
   end
 
   private
+
+  def validate_not_rentable_on_rental_going
+    errors.add(book.title, 'は貸出中です。借りることはできません。') if @rental&.going?
+  end
 
   def validate_not_returnable_on_rental_not_going
     errors.add(book.title, 'をまだ借りていません。') unless @rental&.returned_at.nil?
