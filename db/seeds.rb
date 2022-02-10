f30a6fd9788423787e8bf71b26075c6ad8b7a6fb
大口拓哉 <ookuchitakuya@USERnoMacBook-Pro.local>
2022-02-10 15:53:34 +0900

    社員と書籍のseedデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: '中島滋', furigana: 'ナカジマシゲル')
+User.create!(name: '千葉雄太', furigana: 'チバユウタ')
+User.create!(name: '岸恭輔', furigana: 'キシキョウスケ')
+User.create!(name: '大口拓哉', furigana: 'オオクチタクヤ')
+
+Book.create!(title: '新しいLinuxの教科書')
+Book.create!(title: 'すっきりわかるSQL入門')
+Book.create!(title: 'ゼロからわかるRuby超入門')
+Book.create!(title: 'プロを目指す人のためのRuby入門')
+Book.create!(title: '現場で使えるRuby on Rails 5速習ガイド')
