ae3ca792915770cb967915c163778db4109bd32f
大口拓哉 <ookuchitakuya@USERnoMacBook-Pro.local>
2022-02-17 11:31:06 +0900

    画像ファイルのバリデーションを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,7 @@ class Book < ApplicationRecord
   # 同じ本が２冊以上ある場合、１冊づつ別々で登録するためユニーク制約は設定しない
   validates :title, presence: true
   validates :title, length: { maximum: 30 }
+  validate :validate_image
 
   has_many :rentals, dependent: :destroy
   has_one_attached :image
@@ -15,5 +16,23 @@ class Book < ApplicationRecord
 
   def current_borrower
     going_rental.user
+  end
+
+  def validate_image
+    return unless image.attached?
+    if image.blob.byte_size > 10.megabytes
+      image = nil
+      errors.add(:image, 'ファイルのサイズが大きすぎます')
+    elsif !image?
+      image = nil
+      errors.add(:image, 'ファイルが対応している画像データではありません')
+    end
+    image
+  end
+
+  private
+
+  def image?
+    %w[image/jpg image/jpeg image/gif image/png].include?(image.blob.content_type)
   end
 end
